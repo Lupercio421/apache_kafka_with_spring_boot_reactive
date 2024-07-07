655e99e1729cffbd21ed2e1b1e092b0e0604a0f8
Daniel L. <daniel.llupercio@gmail.com>
2024-07-07 15:58:22 -0400

    adding @Component annotation, but not being explicitly used

--- consumer/src/main/java/com/daniel/wikimedia/consumer/repository/WikiMediaCrudRepository.java
+++ consumer/src/main/java/com/daniel/wikimedia/consumer/repository/WikiMediaCrudRepository.java
@@ -2,6 +2,8 @@ package com.daniel.wikimedia.consumer.repository;
 
 import com.daniel.wikimedia.consumer.MetaDataObject.WikimediaObject;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface WikiMediaCrudRepository extends ReactiveMongoRepository<WikimediaObject, String> {
 }
