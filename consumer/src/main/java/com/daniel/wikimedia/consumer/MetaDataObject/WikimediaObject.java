9eaa3832455ff1812a3b54003c745a78cc418133
Daniel L. <daniel.llupercio@gmail.com>
2024-09-21 17:23:22 -0400

    removing comments

--- consumer/src/main/java/com/daniel/wikimedia/consumer/MetaDataObject/WikimediaObject.java
+++ consumer/src/main/java/com/daniel/wikimedia/consumer/MetaDataObject/WikimediaObject.java
@@ -1,11 +1,5 @@
 package com.daniel.wikimedia.consumer.MetaDataObject;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-// import com.fasterxml.jackson.databind.ObjectMapper; // version 2.11.1
-// import com.fasterxml.jackson.annotation.JsonProperty; // version 2.11.1
-/* ObjectMapper om = new ObjectMapper();
-Root root = om.readValue(myJsonString, Root.class); */
-
 import lombok.Data;
 
 @Data
