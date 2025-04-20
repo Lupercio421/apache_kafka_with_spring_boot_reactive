b616b02852973126494dc27fb46838a81db41de1
Daniel L. <daniel.llupercio@gmail.com>
2025-04-19 23:08:04 -0400

    @Value was not working so created private variables instead

--- producer/src/main/java/com/daniel/wikimedia/producer/stream/WikiMediaStreamConsumer.java
+++ producer/src/main/java/com/daniel/wikimedia/producer/stream/WikiMediaStreamConsumer.java
@@ -2,27 +2,34 @@ package com.daniel.wikimedia.producer.stream;
 
 import com.daniel.wikimedia.producer.producer.WikiMediaProducer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @Slf4j
+@Configuration
 public class WikiMediaStreamConsumer {
+    // The WebClient is used to consume the Wikimedia stream
+
+    //@Value("${project.base-url}")
+    private String baseUrl = "https://stream.wikimedia.org/v2";
+    //@Value("${project.uri}")
+    private String uri = "/stream/recentchange";
+
     private final WebClient webClient;
-
     private final WikiMediaProducer wikiMediaProducer;
-
 
     public WikiMediaStreamConsumer(WebClient.Builder webClientBuilder, WikiMediaProducer wikiMediaProducer) {
         this.webClient = webClientBuilder
-                .baseUrl("https://stream.wikimedia.org/v2")
+                .baseUrl(baseUrl)
                 .build();
         this.wikiMediaProducer = wikiMediaProducer;
     }
 
     public void consumeStreamAndPublish(){
         webClient.get()
-                .uri("/stream/recentchange")
+                .uri(uri)
                 .retrieve()
                 .bodyToFlux(String.class)
                 .subscribe(wikiMediaProducer::sendMessage);
