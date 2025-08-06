06cb955512d9e048c8f53990ea8466a13f3c41e1
Daniel L. <daniel.llupercio@gmail.com>
2025-08-05 23:26:12 -0400

    using V1 logging in Consumer project

--- consumer/src/main/java/com/daniel/wikimedia/consumer/config/WikimediaTopicConfig.java
+++ consumer/src/main/java/com/daniel/wikimedia/consumer/config/WikimediaTopicConfig.java
@@ -1,14 +1,20 @@
 package com.daniel.wikimedia.consumer.config;
 
+import com.dan.logging.LoggingFormatter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+import static com.daniel.wikimedia.consumer.logging.CommandConstants.WIKIMEDIA_CONSUMER_SERVICE_NAME;
+
+@Slf4j
 @Configuration
 public class WikimediaTopicConfig {
     @Bean
     public NewTopic wikiMediaStreamTopic(){
+        log.info(LoggingFormatter.KV_WIKIMEDIA_LOGGING_FORMAT_V1, WIKIMEDIA_CONSUMER_SERVICE_NAME, "WikimediaTopicConfig", "wikiMediaStreamTopicBuilder", "Creating Kafka Topic for Wikimedia Stream");
         return TopicBuilder
                 .name("WikiMedia-Stream")
                 .build();
---
+++ consumer/src/main/java/com/daniel/wikimedia/consumer/logging/CommandConstants.java
@@ -0,0 +1,5 @@
+package com.daniel.wikimedia.consumer.logging;
+
+public class CommandConstants {
+    public final static String WIKIMEDIA_CONSUMER_SERVICE_NAME = "WikiMediaConsumer";
+}
