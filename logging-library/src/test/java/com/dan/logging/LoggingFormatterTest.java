2db4d21a665bff29905cf984f1afeb34d62b661a
Daniel L. <daniel.llupercio@gmail.com>
2025-08-05 23:24:12 -0400

    using BeforeEach

--- logging-library/src/test/java/com/dan/logging/LoggingFormatterTest.java
+++ logging-library/src/test/java/com/dan/logging/LoggingFormatterTest.java
@@ -1,10 +1,18 @@
 package com.dan.logging;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 class LoggingFormatterTest {
+
+    @BeforeEach
+    void setUp() {
+        log.info("Setting up the LoggingFormatterTest class");
+        System.setProperty("wikimedia.infrastructure.environment", "test-environment");
+    }
 
     @Test
     void getSpringActiveProfile() {
