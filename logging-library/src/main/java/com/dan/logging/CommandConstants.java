4e0e5bdd0f5a286ba6ef90cd3cbf375efe95306a
Daniel L. <daniel.llupercio@gmail.com>
2025-08-05 23:25:02 -0400

    using constants throughout logging library

--- logging-library/src/main/java/com/dan/logging/CommandConstants.java
+++ logging-library/src/main/java/com/dan/logging/CommandConstants.java
@@ -2,4 +2,10 @@ package com.dan.logging;
 
 public class CommandConstants {
     protected static final String LOGGING_VERSION_ONE = "1.0";
+    protected static final String WIKIMEDIA_INFRASTRUCTURE_ENVIRONMENT_CONSTANT = "wikimedia.infrastructure.environment";
+    protected static final String SPRING_ACTIVE_PROFILE_CONSTANT = "spring.profiles.active";
+
+    private CommandConstants() {
+        throw new IllegalStateException("Utility class. Not meant to be instantiated.");
+    }
 }
