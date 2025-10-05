360aefe78781aa16a22fae0e6bdaffd6f2d1c9bd
Daniel L. <daniel.llupercio@gmail.com>
2025-10-05 12:46:11 -0400

    logging formatter

--- producer/src/main/java/com/daniel/wikimedia/producer/config/WebClientConfig.java
+++ producer/src/main/java/com/daniel/wikimedia/producer/config/WebClientConfig.java
@@ -1,14 +1,18 @@
 package com.daniel.wikimedia.producer.config;
 
+import com.dan.logging.LoggingFormatter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Slf4j
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient.Builder webClientBuilder(){
+        log.info(LoggingFormatter.WIKIMEDIA_LOGGING_FORMAT_V1, "WikimediaProducerService", "WebClientConfig", "webClientBuilder", "Creating WebClient Builder");
         return WebClient.builder();
     }
 }
